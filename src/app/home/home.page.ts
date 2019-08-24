8359616039c8befde9559cc17e020c3376607b26
christianpaes <christianoliveirapaes3@gmail.com>
2019-08-24 11:47:24 -0300

    commit2

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ModalPage } from '../modal/modal.page';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(public modalController: ModalController) {}
+
+  async openModal(){
+    const modal = await this.modalController.create({
+      component: ModalPage
+    });
+    return await modal.present();
+  }
+
+  }
+
+
---
+++ src/app/modal/modal.page.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalController, AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.page.html',
+  styleUrls: ['./modal.page.scss'],
+})
+export class ModalPage implements OnInit {
+  peso:any;
+  altura:any;
+  imc:any;
+  status:any;
+  constructor(public modalController: ModalController, public alertController: AlertController) { }
+
+  ngOnInit() {
+  }
+  async enviadados(){
+    console.log(this.peso);
+    console.log(this.altura);
+    this.imc=this.peso/((this.altura)*(this.altura));
+    console.log(this.imc);
+    if(this.imc<20){
+      this.status="Abaixo do Peso";
+    }
+    if(this.imc>=20 && this.imc<=24){
+      this.status="Peso Normal";
+    }
+    if(this.imc>=24.1 && this.imc<=29){
+      this.status="Excesso de Peso";
+    }
+    if(this.imc>=29.1 && this.imc<=34){
+      this.status="Obesidade";
+    }
+    if(this.imc>=34.1){
+      this.status="Obesidade Morbida";
+    }
+    console.log(this.status);
+
+    const alert = await this.alertController.create({
+      header: 'Seu índice de massa corporea é: ',
+      subHeader: this.imc,
+      message: this.status,
+      buttons: [{
+        text:'OK',
+        handler:()=>{
+          this.closeModal();
+        }
+      }]
+    });
+
+    await alert.present();
+  }
+
+
+
+  async closeModal(){
+    const onClosedData:string = "Wrapped UP!";
+    await this.modalController.dismiss(onClosedData);
+  }
+
+}
